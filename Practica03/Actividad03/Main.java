d98c3b32a30c4bfba9d67f80a38684181c1f1b68
Juan Jose <ServantOfSatori@gmail.com>
2026-04-20 21:01:51 -0600

    - Terminada la practica 03! - Agregado el ultimo enemigo

--- Practica03/Actividad03/Main.java
+++ Practica03/Actividad03/Main.java
@@ -10,17 +10,20 @@ public class Main
         Botin dropLich = new Botin("Cetro de Hueso", 500);
         Botin dropSlime = new Botin("Núcleo de Caos", 10000);
         Botin dropComun = new Botin("Trapo sucio", 2);
+        Botin dropElder = new Botin("Núcleo de Valstrax", 8000);
 
         //Necesitamos... MONSTRUOS
-        Monstruo m1 = new Monstruo("Lesser Lich", "No-Muerto", 45, 1000, dropLich, true);
-        Monstruo m2 = new Monstruo("Chaos Lord Slime", "Organico", 475, 7500, dropSlime, true);
-        Monstruo m3 = new Monstruo("Zombie", "No-muerto", 5, 25, dropComun, false);
-        Monstruo m4 = new Monstruo("Automata de combate", "Constructo artificial", 17, 45, dropComun, false);
+        Monstruo m1 = new Monstruo("Lesser Lich", "No-Muerto", 150, 45, dropLich, true);
+        Monstruo m2 = new Monstruo("Chaos Lord Slime", "Organico", 475000, 457, dropSlime, true);
+        Monstruo m3 = new Monstruo("Zombie", "No-muerto", 25, 5, dropComun, false);
+        Monstruo m4 = new Monstruo("Automata de combate", "Constructo artificial", 45, 17, dropComun, false);
+        Monstruo m5 = new Monstruo("Valstrax", "Drágon anciano", 75000, 40, dropElder, true);
 
         miBestiario.agregarMonstruo(m1);
         miBestiario.agregarMonstruo(m2);
         miBestiario.agregarMonstruo(m3);
         miBestiario.agregarMonstruo(m4);
+        miBestiario.agregarMonstruo(m5);
 
         for(Monstruo m : miBestiario.getLista())
         {
