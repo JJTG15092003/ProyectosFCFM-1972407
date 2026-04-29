28c1ec0fe8ec36adff3e02a7fa150eff6fed394e
Juan Jose <ServantOfSatori@gmail.com>
2026-04-28 20:09:43 -0600

    - Modificado Bestiario.java para agregar las interfaces nuevas de accion y conversador

--- Practica06/Actividad06/Bestiario.java
+++ Practica06/Actividad06/Bestiario.java
@@ -29,4 +29,35 @@ public class Bestiario
             System.out.println(m);
         }
     }
+
+    //Interaccion de conversacion
+    public void intentarDiplomacia()
+    {
+        System.out.println("\n--- Iniciando Fase de Diplomacia ---");
+        for (Monstruo m : listaMonstruos)
+        {
+            if (m instanceof Conversador)
+            {
+                Conversador c = (Conversador) m;
+                System.out.println(m.getNombre() + " responde: " + c.persuadir());
+            } else
+            {
+                System.out.println(m.getNombre() + " no parece interesado en hablar.");
+            }
+        }
+    }
+
+    //Fase de combate
+    public void ejecutarFaseDeAccion()
+    {
+        System.out.println("\n--- Iniciando Fase de Acción de Combate ---");
+        for (Monstruo m : listaMonstruos)
+        {
+            if (m instanceof Accion)
+            {
+                Accion a = (Accion) m;
+                System.out.println(a.atacar());
+            }
+        }
+    }
 }
