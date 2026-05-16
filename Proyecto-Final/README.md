c7ebcff68ab5eca566ab9ad907595ed79419800f
Juan Jose <ServantOfSatori@gmail.com>
2026-05-15 22:02:30 -0600

    - Creado el README.md - Añadido el Proyecto-Final-1.0-SNAPSHOT como una realese

--- Proyecto-Final/README.md
+++ Proyecto-Final/README.md
@@ -1,1 +1,3 @@
-Aqui va texto
+Para ejecutar el codigo, ejecutar: Proyecto-Final-1.0-SNAPSHOT.jar
+
+En EXTRUCTURA.md esta guardado el diseño que me dio la IA sobre el cual me base para trabajar
---
+++ Proyecto-Final/src/main/java/com/example/gympos/app/Launcher.java
@@ -0,0 +1,8 @@
+package com.example.gympos.app;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Esto llama al main de tu App de JavaFX sin heredar de Application
+        MainApp.main(args);
+    }
+}
